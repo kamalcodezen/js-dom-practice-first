5a9500c9a16c72afbd1ec62bdf00db4a6d67a6c0
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-14 01:58:42 +0530

    querySelecto practice

--- javaScript/first.js
+++ javaScript/first.js
@@ -9,12 +9,23 @@ for (const tagName of tagNames) {
 
 
 const listItem = document.getElementsByClassName("list");
-console.log(listItem);
+// console.log(listItem);
 for (let list of listItem) {
-    console.log(list.innerText);
+    // console.log(list.innerText);
 }
 
 
 
-const company = document.getElementById("food");
-console.log(company);
+const foods = document.getElementById("food");
+// console.log(foods);
+
+
+const foods1 = document.querySelector("#food")
+// console.log(foods1.innerText);
+
+
+const foodsItem = document.querySelectorAll(".list");
+console.log(foodsItem)
+for (let food of foodsItem) {
+    console.log(food.innerText)
+}

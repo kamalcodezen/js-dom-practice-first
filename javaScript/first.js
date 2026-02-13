6c09de4a52a10d2ac0e568357b252a7717203ab6
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-14 01:24:46 +0530

    className access

--- javaScript/first.js
+++ javaScript/first.js
@@ -4,5 +4,13 @@ const tagNames = document.getElementsByTagName("h1");
 // console.log(tagNames);
 
 for (const tagName of tagNames) {
-    console.log(tagName);
+    // console.log(tagName);
 }
+
+
+const listItem = document.getElementsByClassName("list");
+console.log(listItem);
+for (let list of listItem) {
+    console.log(list.innerText);
+}
+

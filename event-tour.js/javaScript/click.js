104493dc8de8c06b04ce6f5349d3b491f95fc5b8
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-15 15:13:23 +0530

    option 1 2 click event handler practice

--- event-tour.js/javaScript/click.js
+++ event-tour.js/javaScript/click.js
@@ -1,2 +1,14 @@
 // option  2 click handler
 
+function makeYellow() {
+    document.body.style.background = "yellow";
+}
+
+
+// option 3 click handle
+
+const makeYellowBtn = document.getElementById("make-yellow-btn");
+
+makeYellowBtn.onclick = function makeRed() {
+    document.body.style.display = "none";
+}

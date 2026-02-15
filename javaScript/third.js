616bdb12cc3791d15f68fbba2c6216744212ecfc
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-15 13:00:10 +0530

    some design in js

--- javaScript/third.js
+++ javaScript/third.js
@@ -1,6 +1,4 @@
 
-
-
 // section create
 
 const newSection = document.createElement("section");
@@ -9,19 +7,34 @@ newSection.setAttribute("id", "cities");
 // h1 create
 const headerEl = document.createElement("h1");
 headerEl.innerText = "All cities";
+headerEl.style.color = "red";
+headerEl.style.fontSize = "60px";
+headerEl.style.textAlign = "center";
+headerEl.style.fontWeight = "700";
 
 // appendChild
 newSection.appendChild(headerEl);
 
 
 const citiesName = document.createElement("ul");
+citiesName.style.fontSize = "40px";
+citiesName.style.textAlign = "center";
+
+
 
 const list1 = document.createElement("li");
 list1.innerText = "delhi";
+list1.style.listStyle = "none"
+
 const list2 = document.createElement("li");
 list2.innerText = "mumbai";
+list2.style.listStyle = "none"
+
 const list3 = document.createElement("li");
 list3.innerText = "kolkata";
+list3.style.listStyle = "none"
+
+
 
 citiesName.appendChild(list1);
 citiesName.appendChild(list2);
@@ -32,3 +45,4 @@ newSection.appendChild(citiesName);
 
 
 document.body.appendChild(newSection);
+
